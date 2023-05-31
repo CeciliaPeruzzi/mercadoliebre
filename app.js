603042c53e0345651c708eb017fca2301070325a
CeciliaPeruzzi <ceciliaperuzzi2@hotmail.com>
2023-05-31 16:07:21 -0300

    apps

--- app.js
+++ app.js
@@ -9,3 +9,14 @@ app.listen(process.env.PORT || 3000, () => {
     console.log('Servidor corriendo en el puerto 3000')
 })
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+})
