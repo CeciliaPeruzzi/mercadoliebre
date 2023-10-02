a76820ba7788e3eecaee72d42d38a78bd1fb8d30
CeciliaPeruzzi <ceciliaperuzzi2@hotmail.com>
2023-10-01 21:47:31 -0300

    check

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
-const express= require ('express')
-const path = require('path')
+const express= require ('express');
+const path = require('path');
+const app = express();
 
-const app = express ()
+
+
+
 
 app.use(express.static('Public'));
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log('Servidor corriendo en el puerto 3000')
+    console.log('Servidor corriendo en el puerto 3000');
 })
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/Views/home.html'))
+    res.sendFile(path.join(__dirname, './Views/home.html'));
 })
 
 app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, '/Views/register.html'))
+    res.sendFile(path.join(__dirname, './Views/register.html'));
 })
 
 app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, '/Views/login.html'))
+    res.sendFile(path.join(__dirname, './Views/login.html'));
 })
